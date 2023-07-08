3c01441ba574331d7fa3e340f77f2c958705a02f
SenorMeowzors <ravingkeroro@gmail.com>
2023-07-08 06:40:01 -0500

    Overhaul camera and movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,36 +5,75 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rb;
-    [SerializeField] private float movespeed;
-    [SerializeField] private Rigidbody rigidbody_e;
-    private Vector2 turn;
-    public float sensitivity = 500f;
-    private Vector2 movement;
-    
-    // Start is called before the first frame update
-    private void Awake()
+    [SerializeField]
+    Animator animator;
+
+    public float moveSpeed = 10.0f;
+    public float turnSpeed = 10.0f;
+
+    public Vector2 input;
+    Camera mainCamera;
+    Rigidbody rb;
+
+    Vector3 targetDirection;
+    private Quaternion freeRotation;
+
+    void Start()
     {
+        mainCamera = Camera.main;
         rb = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
-        
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        turn += new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * sensitivity;
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
-        rb.velocity = transform.forward * movement.y * movespeed * Time.deltaTime + transform.right * movement.x * movespeed * Time.deltaTime;
-        rb.velocity=new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        UpdateMovement();
+        UpdateAnimation();
     }
 
-    private void OnMove(InputValue val)
+    private void UpdateMovement()
     {
-        movement = val.Get<Vector2>();
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        input = new Vector2(x, y);
+
+        var m_CamForward = Vector3.Scale(mainCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+        var dirRight = Vector3.Scale(mainCamera.transform.right, new Vector3(1, 0, 1)).normalized;
+        var m_Move = (input.y * m_CamForward) + (input.x * dirRight);
+
+        Vector3 force = (m_Move * moveSpeed);
+        force = Vector3.ProjectOnPlane(force, Vector3.up) * input.magnitude;
+
+        rb.velocity = force;
     }
-    private void OnPull()
+
+    private void UpdateAnimation()
     {
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+    }
+
+    private void LateUpdate()
+    {
+        FaceDirection();
+    }
+
+    void FaceDirection()
+    {
+        var forward = mainCamera.transform.forward;
+        var right = mainCamera.transform.right;
+
+        targetDirection = input.x * right + input.y * forward;
         
+        if (input != Vector2.zero && targetDirection.magnitude > 0.1f)
+        {
+            Vector3 lookDirection = targetDirection.normalized;
+            freeRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+            var diferenceRotation = freeRotation.eulerAngles.y - transform.eulerAngles.y;
+            var eulerY = transform.eulerAngles.y;
+
+            if (diferenceRotation < 0 || diferenceRotation > 0) eulerY = freeRotation.eulerAngles.y;
+            var euler = new Vector3(0, eulerY, 0);
+
+            rb.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * Time.deltaTime);
+        }
     }
 }
